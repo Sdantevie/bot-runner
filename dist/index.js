e5ac4702656bc69bdf694371d9d34f1f9d212748
Steven Daniel <dstevie9292@gmail.com>
2022-04-17 07:17:37 +0100

    progress

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const tradeManager_1 = require("./bot-engine/tradeManager");
 const priceStoreManager_1 = require("./bot-engine/priceStoreManager");
+const mongoose_1 = __importDefault(require("mongoose"));
+const constants_1 = require("./utils/constants");
 const port = process.env.PORT || 3515;
 (async () => {
-    // await mongoose.connect(devDbUrl);
+    await mongoose_1.default.connect(constants_1.devDbUrl);
     (0, tradeManager_1.botRunner)();
     (0, priceStoreManager_1.priceStoreRunner)();
 })();
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { botRunner } from './bot-engine/tradeManager';
 import { priceStoreRunner } from './bot-engine/priceStoreManager';
+import mongoose from 'mongoose';
+import { devDbUrl } from './utils/constants';
 
 const port = process.env.PORT || 3515;
 
 (async () => {
 
-  // await mongoose.connect(devDbUrl);
+  await mongoose.connect(devDbUrl);
   botRunner();
   priceStoreRunner();
 
